75ced552bc407a2e77f2fe73f4797a1ddfd92aaa
Siarhei Chekan <unclesamsch@gmail.com>
2021-05-05 00:02:43 +0300

    fixed bugs in using localStorage to save theme after page reload

--- src/frontend/app/components/header/components/theme-switcher/component.js
+++ src/frontend/app/components/header/components/theme-switcher/component.js
@@ -17,61 +17,79 @@ export function ThemeSwitcher() {
 
     div.append(input, labelDark);
 
-    localStorage.setItem('theme', 'light');
+    const theme = localStorage.getItem('theme');
+
+    if (theme === 'dark') {        
+        setDarkTheme();
+    } else {
+        localStorage.setItem('theme','light');
+        setLightTheme();
+    }
 
     const switcher = div.querySelector('#themeSwitcher');
     switcher.addEventListener('click', changeTheme);
+
+    if (theme === 'dark') {
+        switcher.setAttribute('checked', 'true');
+    }
 
     return div;
 }
 
 function setDarkTheme() {
-    const header = document.querySelector('header');
-    const nav = header.querySelector('nav');
-    const main = document.querySelector('main');
-    const table = main.querySelector('table');
-    const buttons = table.querySelectorAll('button');
-    const footer = document.querySelector('footer');
-    const divTable = document.querySelector('div[class*="table-responsive"]');
+    const divRoot = new Promise((resolve) => {
+        resolve('App');
+    }).then((result) => {
+        const header = document.querySelector('header');
+        const nav = header.querySelector('nav');
+        const main = document.querySelector('main');
+        const table = main.querySelector('table');
+        const buttons = table.querySelectorAll('button');
+        const footer = document.querySelector('footer');
+        const divTable = document.querySelector('div[class*="table-responsive"]');
 
-    localStorage.setItem('theme', 'dark');
+        localStorage.setItem('theme', 'dark');
 
-    header.classList.add('bg-dark', 'text-white');
-    nav.classList.add('navbar-dark');
-    divTable.classList.add(stylesTable.fixedHeightDark);
-    table.classList.add('table-dark', stylesTable.tableDarkOpacity);
-    buttons.forEach(button => {
-        button.classList.add(stylesButton.openTaskDark, stylesButton.btnLinkDark);
-        button.classList.remove(stylesButton.openTask, stylesButton.btnLink);
-    });
-    footer.classList.add('bg-dark', 'text-white');
+        header.classList.add('bg-dark', 'text-white');
+        nav.classList.add('navbar-dark');
+        divTable.classList.add(stylesTable.fixedHeightDark);
+        table.classList.add('table-dark', stylesTable.tableDarkOpacity);
+        buttons.forEach(button => {
+            button.classList.add(stylesButton.openTaskDark, stylesButton.btnLinkDark);
+            button.classList.remove(stylesButton.openTask, stylesButton.btnLink);
+        });
+        footer.classList.add('bg-dark', 'text-white');
 
-    nav.classList.remove('navbar-light', 'bg-light');
+        nav.classList.remove('navbar-light', 'bg-light');
+    });    
 }
 
 function setLightTheme() {
-    const header = document.querySelector('header');
-    const nav = header.querySelector('nav');
-    const main = document.querySelector('main');
-    const table = main.querySelector('table');
-    const buttons = table.querySelectorAll('button');
-    const footer = document.querySelector('footer');
-    const divTable = document.querySelector('div[class*="table-responsive"]');
+    const divRoot = new Promise((resolve) => {
+        resolve('App');
+    }).then((result) => {
+        const header = document.querySelector('header');
+        const nav = header.querySelector('nav');
+        const main = document.querySelector('main');
+        const table = main.querySelector('table');
+        const buttons = table.querySelectorAll('button');
+        const footer = document.querySelector('footer');
+        const divTable = document.querySelector('div[class*="table-responsive"]');
 
+        localStorage.setItem('theme', 'light');
+        
+        nav.classList.add('navbar-light', 'bg-light');
 
-    localStorage.setItem('theme', 'light');
-    
-    nav.classList.add('navbar-light', 'bg-light');
-
-    header.classList.remove('bg-dark', 'text-white');
-    nav.classList.remove('navbar-dark');
-    divTable.classList.remove(stylesTable.fixedHeightDark);
-    table.classList.remove('table-dark', stylesTable.tableDarkOpacity);
-    buttons.forEach(button => {
-        button.classList.remove(stylesButton.openTaskDark, stylesButton.btnLinkDark);
-        button.classList.add(stylesButton.openTask, stylesButton.btnLink);
-    });
-    footer.classList.remove('bg-dark', 'text-white');
+        header.classList.remove('bg-dark', 'text-white');
+        nav.classList.remove('navbar-dark');
+        divTable.classList.remove(stylesTable.fixedHeightDark);
+        table.classList.remove('table-dark', stylesTable.tableDarkOpacity);
+        buttons.forEach(button => {
+            button.classList.remove(stylesButton.openTaskDark, stylesButton.btnLinkDark);
+            button.classList.add(stylesButton.openTask, stylesButton.btnLink);
+        });
+        footer.classList.remove('bg-dark', 'text-white');
+    });    
 }
 
 function changeTheme({
